f055e0e50caa82a899fccae9344c765b4e238b58
osheina <150192743+osheina@users.noreply.github.com>
2024-02-20 22:44:13 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -29,12 +29,18 @@ async def send_echo(message: Message):
 
     russian = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 
                'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 
-               'Ч', 'Ш', 'Щ', 'Ы', 'Ъ', 'Э', 'Ю', 'Я']
+               'Ч', 'Ш', 'Щ', 'Ы', 'Ъ', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 
+               'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 
+               'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ы', 
+               'ъ', 'э', 'ю', 'я']
     latin = ['A', 'B', 'V', 'G', 'D', 'E', 'E', 'ZH', 'Z', 'I', 'I', 'K', 
              'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'KH', 'TS',
-             'CH', 'SH', 'SHCH', 'Y', 'IE', 'E', 'IU', 'IA']
+             'CH', 'SH', 'SHCH', 'Y', 'IE', 'E', 'IU', 'IA', 'a', 'b', 'v', 
+             'g', 'd', 'e', 'e', 'zh', 'z', 'i', 'i', 'k', 'l', 'm', 'n', 
+             'o', 'p', 'r', 's', 't', 'u', 'f', 'kh', 'ts', 'ch', 'sh', 
+             'shch', 'y', 'ie', 'e', 'iu', 'ia']
     
-    name_translit = ''.join([latin[russian.index(i.upper())] if i.upper() in russian else '' for i in text])
+    name_translit = ''.join([latin[russian.index(i)] if i in russian else i for i in text])
 
     await bot.send_message(chat_id=user_id, text=name_translit)
 
